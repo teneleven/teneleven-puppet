b52e579537e7f6c330845929c435c3bed683b965
Michael Mackus <michaelmackus@gmail.com>
2016-07-29 14:43:13 -0700

    Add docker::commit resource

---
+++ manifests/docker/commit.pp
@@ -0,0 +1,17 @@
+define teneleven::docker::commit (
+  $container = $title,
+  $tag       = undef # defaults to $teneleven::params::docker_prefix/$container
+) {
+
+  $real_tag = $tag ? {
+    undef   => "${teneleven::params::docker_prefix}/${container}",
+    default => $tag
+  }
+
+  /* commit container */
+  exec { "commit-${container}":
+    command     => "docker commit ${container} ${real_tag}",
+    provider    => 'shell'
+  }
+
+}

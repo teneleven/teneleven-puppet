9d104f2a6569e66379b846de5414fb479f41d41b
Michael Mackus <michaelmackus@gmail.com>
2016-08-11 09:27:09 -0700

    Initialize helpful defaults

--- manifests/elastic_search.pp
+++ manifests/elastic_search.pp
@@ -1,15 +1,27 @@
 class teneleven::elastic_search (
   $settings = {},
 
+  $instances = {} /* not required, will setup a default instance */
+) inherits teneleven::params {
+
   $default_settings = {
     java_install => true,
-    manage_repo  => true
+    manage_repo  => true,
+    repo_version => '2.x'
   }
-) inherits teneleven::params {
 
   create_resources('class', { 'elasticsearch' => merge(
     $default_settings,
     $settings
   )})
+
+  $instances.each |$name, $instance| {
+    create_resources('elasticsearch::instance', { $name => $instance })
+  }
+
+  if (empty($instances)) {
+    /* setup default instance */
+    elasticsearch::instance { 'default': }
+  }
 
 }
